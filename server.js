21281716af441d5c016ea5fbdb0027c8da45e50f
Awanish Sharma <awanish.sharma.5683@gmail.com>
2025-01-18 23:57:42 +0530

    update

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ import connecCloudinary from "./config/cloudinary.js";
 import adminRouter from "./routes/adminRoute.js";
 import userRouter from "./routes/userRoute.js";
 import doctorRouter from "./routes/doctorRoute.js";
-import mongoose from "mongoose";
 
 
 const app = express();
@@ -18,38 +17,7 @@ connecCloudinary();
 
 // middelwares
 app.use(express.json());
-// app.use(cors);
-
-
-// List of allowed origins (make sure you include your local development URL)
-const allowedOrigins = [
-  'http://localhost:5173', // Add your frontend URL for local dev
-  'https://startling-kangaroo-bcc26e.netlify.app/' // Add your Netlify production URL
-];
-
-// CORS options
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      // Allow the request if the origin is in the allowed list
-      callback(null, true);
-    } else {
-      // Reject the request if the origin is not allowed
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true, // If you're using cookies or credentials
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow necessary HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Add any custom headers you use
-};
-
-// Apply CORS middleware to all routes
-app.use(cors(corsOptions));
-
-// Handle preflight (OPTIONS) requests for all routes
-app.options('*', cors(corsOptions));
-
-
+app.use(cors);
 
 
 
